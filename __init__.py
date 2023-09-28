13aad8eb878c5be77f6b981dabb17f83db5efaa9
Vlad Stan <stan.v.vlad@gmail.com>
2023-09-28 10:18:36 +0300

    chore: revert path changes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
@@ -10,13 +11,14 @@ usermanager_ext: APIRouter = APIRouter(prefix="/usermanager", tags=["usermanager
 usermanager_static_files = [
     {
         "path": "/usermanager/static",
+        "app": StaticFiles(directory="lnbits/extensions/usermanager/static"),
         "name": "usermanager_static",
     }
 ]
 
 
 def usermanager_renderer():
-    return template_renderer(["usermanager/templates"])
+    return template_renderer(["lnbits/extensions/usermanager/templates"])
 
 
 from .views import *  # noqa
